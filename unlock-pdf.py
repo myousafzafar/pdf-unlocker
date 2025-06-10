e6a3ec40888246ab12dc334e37e6cfb18d97a5d1
Muhammad Yousaf Zafar <83083964+myousafzafar@users.noreply.github.com>
2025-06-10 08:48:45 +0500

    Update unlock-pdf.py

--- unlock-pdf.py
+++ unlock-pdf.py
@@ -7,7 +7,7 @@ output_pdf = "unlocked.pdf"
 # Brute-force 4-digit passwords: 0000 to 9999
 # To try more digits, increase the range (e.g., range(100000) for 5-digit, and so on...)
 for i in range(10000):
-    password = f"{i:04d}"  # Format number as 4-digit string (e.g., 0001, 0423)
+    password = f"{i:04d}"  # Format number as 4-digit string (e.g., 0001, 0423). For 5-digit string, use '{i:05d}', and so on...
     try:
         # Attempt to open the PDF with the current password
         with pikepdf.open(locked_pdf, password=password) as pdf:
